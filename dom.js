8f34c3333a68388cc1950b8f3296952402d0a3d2
Tushar Shinde <tusharshindets086@gmail.com>
2024-12-10 12:56:19 +0530

    Resolved merge conflicts

---
+++ dom.js
@@ -0,0 +1,20 @@
+// console.log(document)
+console.dir(document)
+console.dir(document.all);
+console.log( document.getElementById("description") )
+console.log( document.getElementsByTagName('p') )
+console.log( document.getElementsByClassName("box") );
+
+console.dir(document.all[8]);
+console.dir(document.all[8].innerText);
+console.dir(document.all[8].innerText = " Peter Parker");
+
+console.log(document.querySelector('p'))
+console.log(document.querySelector("#description"))
+console.log(document.querySelector(".box"))
+console.log(document.querySelectorAll("div a"))
+console.log(document.querySelectorAll("h1"))
+console.log(document.querySelectorAll(".box"))
+
+
+console.log

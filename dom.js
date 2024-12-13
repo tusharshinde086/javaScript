85eb2fcba5dfd1c831ebbcb011e51504124be32a
Tushar Shinde <tusharshindets086@gmail.com>
2024-12-13 15:26:20 +0530

    style in js

--- dom.js
+++ dom.js
@@ -20,3 +20,54 @@ console.log(document.querySelectorAll(".box"))
 let para =document.querySelector("p");
 console.log(para);
 console.dir(para);
+//para.innerText= 'abcd'
+console.log(para);
+//para.innerText= 'Hi , I am a'
+console.log(para);
+let heading = document.querySelector('h1')
+console.log(heading);
+//console.log(heading.innerHTML ='<u>$(heading.innerText)</u>');
+let img = document.querySelector('img');
+//img.getAttribute('id')
+//img.setAttribute('id','spiderman_img')
+//img.setAttribute('src',"assets/creation_1.png")
+// img.getAttribute('class')
+// img.setAttribute('class','image')
+// img.getAttribute('class')
+heading.style.color="blue";
+//heading.style.color="green"
+heading.style.backgroundColor="yellow"
+//heading.style.backgroundColor="red"
+let links = document.querySelectorAll('.box a')
+console.log(links);
+
+for(let i=0 ; i<links.length;i++){
+    links[i].style.color = "blue";
+ }
+
+ for(link of links) { 
+    link.style.color="purple" 
+}
+
+// Manipulating Style ********
+
+let box = document.querySelector('.box')
+console.dir(box.style)
+
+//img.classList
+
+//heading.classList
+//heading.classList.add("abc");
+let head = document.querySelector('h2');
+head.classList.add('green')
+console.dir(head.classList)
+
+head.classList.remove('green')
+console.dir(head.classList.contains('underline'))
+head.classList.toggle('green')
+head.classList.toggle('green')
+
+
+
+
+

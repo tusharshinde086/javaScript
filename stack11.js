8544d44443df2f49903d271eb7ace3d4f363dba4
Tushar Shinde <tusharshindets086@gmail.com>
2025-02-04 15:04:34 +0530

    js promises

--- stack11.js
+++ stack11.js
@@ -88,44 +88,69 @@ console.log("-----------------------------------");
 
 //  promises  
 
-function savetoDB(data, success, failure) {
-    let internetSpeed = Math.floor(Math.random() * 10) + 1;
-    console.log("Your speed:", internetSpeed);
+// function savetoDB(data, success, failure) {
+//     let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//     console.log("Your speed:", internetSpeed);
 
-    if (internetSpeed > 4) {
-        success(data);
-    } else {
-        failure();
-    }
-}
+//     if (internetSpeed > 4) {
+//         success(data);
+//     } else {
+//         failure();
+//     }
+// }
 
-setTimeout(() => {
-    savetoDB("tushar shinde data",
-        (data) => {
-            console.log("Your data was successfully saved:", data);
+// setTimeout(() => {
+//     savetoDB("tushar shinde data",
+//         (data) => {
+//             console.log("Your data was successfully saved:", data);
 
-            // Calling savetoDB again inside success callback
-            savetoDB("hello world",
-                (data) => {
-                    console.log("Your second success 2 data was saved:", data);
-                    savetoDB("new data",
-                        (data)=>{
-                            console.log("Your third success 3  data was saved:", data);
-                        },()=>{
-                            console.log("Weak connection, third data not saved.");
-                        }
-                     ) 
+//             // Calling savetoDB again inside success callback
+//             savetoDB("hello world",
+//                 (data) => {
+//                     console.log("Your second success 2 data was saved:", data);
+//                     savetoDB("new data",
+//                         (data)=>{
+//                             console.log("Your third success 3  data was saved:", data);
+//                         },()=>{
+//                             console.log("Weak connection, third data not saved.");
+//                         }
+//                      ) 
 
-                },() => {
-                    console.log("Weak connection, second data not saved.");
-                }
-            );
-        },() => {
-            console.log("Weak connection, first data not saved.");
-        }
-    );
+//                 },() => {
+//                     console.log("Weak connection, second data not saved.");
+//                 }
+//             );
+//         },() => {
+//             console.log("Weak connection, first data not saved.");
+//         }
+//     );
+// }, 12000);
+
+function savetoDB(data) {
+    return new Promise((resolve, reject) => {
+      let internetSpeed = Math.floor(Math.random() * 10) + 1;
+      console.log("Your speed:", internetSpeed);
+      
+      if (internetSpeed > 4) {
+          resolve(`Success! Data was saved: ${data}`);
+      } else {
+          reject("Weak connection, data not saved.");
+      }
+    });
+  }
+  
+  // Call savetoDB and handle success/failure
+  setTimeout(() => {
+    
+  savetoDB("tushar shinde")
+    .then((message) => {
+      console.log(message);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
 }, 12000);
-
 
 console.log("-----------------------------------");
 

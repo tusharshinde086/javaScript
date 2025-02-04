f64ee1f72ff051bda9bf1ecc292dff65c66244b9
Tushar Shinde <tusharshindets086@gmail.com>
2025-02-04 13:44:20 +0530

    js callback nesting  hell

--- stack11.js
+++ stack11.js
@@ -43,3 +43,46 @@ setTimeout( function () {
 
 console.log("hello ....") ;
 
+//callback hell 
+
+console.log("-----------------------------------");
+
+let h1 = document.querySelector("h1");
+
+function changeColor(color,delay,nextColorChange) {
+    setTimeout(()=>{
+        h1.style.color = color;
+        console.log("color chaged to " , color );
+       if(nextColorChange) nextColorChange();
+    },delay);
+}
+
+setTimeout(() => changeColor("red"), 2000);
+setTimeout(() => changeColor("orange"), 3000);
+changeColor("green", 4000);
+changeColor("yellow", 5000);
+
+//callback nesting 
+
+changeColor("purple", 6000,()=> {
+    changeColor("gray",1000, ()=> {
+        changeColor("white",1000,()=> {
+            changeColor("lime",1000,()=> {
+                changeColor("maroon",1000);
+            });
+        });
+    });
+}); 
+
+setTimeout(() => {
+    h1.style.color = "blue";
+    console.log("color chaged to blue" );
+}, 11000);
+
+setTimeout(() => {
+console.log("-----------------------------------");
+}, 11000);
+
+
+console.log("-----------------------------------");
+
